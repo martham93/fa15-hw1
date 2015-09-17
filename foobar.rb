9ff7c058cc0e4fbad05bb78959506dc4f2675d64
martham93 <marthamorrissey@berkeley.edu>
2015-09-17 14:52:09 -0700

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,8 @@
 class Foobar
-  # Q4 CODE HERE
+  def self.baz(a)
+  	b = a.map{|a| a.to_i}.uniq.map{|a| a + 2}.keep_if{|a| a % 2 == 0}.delete_if{|a| a > 10}.inject(:+)
+  	b
+  end
 end
+
+
--- hw1.rb
+++ hw1.rb
@@ -1,17 +1,16 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  (a + b) * (a +b)
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+	a.map {|a| a + 1}.sort
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+   first_name + " " + last_name
 end
 
 def blockin_time(a)
-  # DO NOT EDIT THIS CODE BELOW
   require './foobar'
-  Foobar.baz a
+ Foobar.baz a
 end
